206df023205d68676ba311a1a540e15aa14bf7d0
czf <czf201031>
2023-05-24 08:48:28 +0800

    axios基础封装

--- src/main.js
+++ src/main.js
@@ -5,10 +5,16 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-
+import { getCategory } from '@/apis/testApi'
+ 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+getCategory().then(res =>{
+    console.log(res)
+})
+
