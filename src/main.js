aa5bd593521bf035d70afdadb00f7767b8cffa73
czf <czf201031>
2023-05-24 14:44:00 +0800

    '静态资源的提交'

--- src/main.js
+++ src/main.js
@@ -6,6 +6,9 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import { getCategory } from '@/apis/testApi'
+
+// 引入初始化样式文件
+import '@/styles/common.scss'
  
 const app = createApp(App)
 
