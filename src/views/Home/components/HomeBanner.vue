874936416fa67e4ca45b9d6a5e5ba87ad1ebe2e7
czf <czf201031>
2023-05-29 21:40:26 +0800

    'banne的事项'

--- src/views/Home/components/HomeBanner.vue
+++ src/views/Home/components/HomeBanner.vue
@@ -1,13 +1,40 @@
+<script setup>
+import{ getBannerApi } from '@/apis/home'
+import{ ref } from 'vue'
+import { onMounted } from 'vue';
+
+const bannerList = ref([])
+const getBanner = async ()=> {
+  const res = await getBannerApi()
+  bannerList.value = res.result;
+  // console.log(res)
+}
+ onMounted(() => {
+  getBanner()
+ })
+</script>
 <template>
-  i am banner
+  <div class="home-banner">
+    <el-carousel height="500px">
+      <el-carousel-item v-for="item in bannerList" :key="item.id">
+        <img :src="item.imgUrl"/>
+      </el-carousel-item>
+    </el-carousel>
+  </div>
 </template>
 
-<script>
-export default {
+<style scoped lang='scss'>
+.home-banner {
+  width: 1240px;
+  height: 500px;
+  position: absolute;
+  left: 0;
+  top: 0;
+  z-index: 98;
 
+  img {
+    width: 100%;
+    height: 500px;
+  }
 }
-</script>
-
-<style>
-
 </style>
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -7,25 +7,21 @@ import HomeCategory from "./components/HomeCategory.vue"
 import HomeNav from "./components/HomeNav.vue"
 import HomeProduct from "./components/HomeProduct.vue"
 import HomeHot from "./components/HomeHot.vue"
-
-
 </script>
 
 <template>
     <RouterView/>
 
-    <div class="conntainer">
-        <div>
-            <HomeCategory/>
-        </div>
-       <div>
+    <div class="container">
+        <HomeCategory/>
         <HomeBanner/>
-       </div>
-        
     </div>
     <HomeNav/>
-    
+    <HomeHot/>
     <HomeProduct/>
     
 
 </template>
+<style>
+
+</style>
