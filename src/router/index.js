525f78ff5d93e0d3fd10abb514a2d309a9236d47
czf <czf201031>
2023-07-04 20:21:54 +0800

    '新鲜好物的商品详情的认识'

--- src/router/index.js
+++ src/router/index.js
@@ -8,8 +8,7 @@ import Layout from '@/views/Layout/index.vue'
 import Category from '@/views/Category/index.vue'
 import  Home from '@/views/Home/index.vue'
 import subCategory from '@/views/subCategory/index.vue'
-
-
+import Detail from '@/views/Detail/index.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -35,6 +34,10 @@ const router = createRouter({
         {
           path: 'category/sub/:id',//这里少加了一个/导致路径找不到
           component: subCategory
+        },
+        {
+          path: 'detail/:id',//这里少加了一个/导致路径找不到
+          component: Detail
         },
       ]
     },
