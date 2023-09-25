19029064ff00c4e439194bdd29b443b54330a463
czf <czf201031>
2023-09-25 08:38:47 +0800

    '放大镜效果'

--- src/components/ImageView/index.vue
+++ src/components/ImageView/index.vue
@@ -1,5 +1,6 @@
 <script setup>
-import {ref} from 'vue'
+import {ref,watch} from 'vue'
+import { useMouseInElement } from '@vueuse/core'
 // 图片列表
 const imageList = [
   "https://yanxuan-item.nosdn.127.net/d917c92e663c5ed0bb577c7ded73e4ec.png",
@@ -14,6 +15,30 @@ const enterhandlwe = (i) =>{
     activieIndex.value = i
 
 }
+const target = ref(null)
+const left = ref(0)
+const top = ref(0)
+
+
+const { elementX,elementY , isOutside } = useMouseInElement(target)
+watch([elementX,elementY],() => {
+  // 有效范围内控制滑块的距离
+  if(elementX.value>100&&elementX.value<300){
+    left.value=elementX.value-100
+  }
+  if(elementY.value>100&&elementY.value<300){
+    top.value=elementY.value-100
+  }
+  if(elementX.value>300){left.value=200}
+  if(elementX.value<100){left.value=0}
+
+  if(elementY.value>300){top.value=200}
+  if(elementY.value<100){top.value=0}
+
+
+})
+
+
 
 </script>
 
@@ -24,7 +49,7 @@ const enterhandlwe = (i) =>{
     <div class="middle" ref="target">
       <img :src="imageList[activieIndex]" alt="" />
       <!-- 蒙层小滑块 -->
-      <div class="layer" :style="{ left: `0px`, top: `0px` }"></div>
+      <div class="layer" :style="{ left: `${left}px`, top: `${top}px` }"></div>
     </div>
     <!-- 小图列表 -->
     <ul class="small">
